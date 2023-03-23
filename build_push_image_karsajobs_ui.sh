a550447dc1e8feb898c2bdb4cea30e94fbe195d3
taqwimOnehub <taqwimcore@outlook.com>
2023-03-24 06:27:39 +0700

    add build.sh

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# Build Docker image dari Dockerfile
+docker build -t karsajobs-ui:latest .
+
+# Using personal access token to GitHub Package container registry
+echo "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" | docker login ghcr.io -u taqwimonehub --password-stdin
+
+# Push image ke GitHub Package container registry
+docker tag karsajobs-ui ghcr.io/taqwimonehub/karsajobs-ui:latest
+docker push ghcr.io/taqwimonehub/karsajobs-ui
