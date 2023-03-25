32ea1cab8f8e066478199ccd979e9b1cd9d7154b
taqwimOnehub <taqwimcore@outlook.com>
2023-03-25 08:09:44 +0700

    add env node

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-
 # Build Docker image dari Dockerfile
 docker build -t karsajobs-ui:latest .
 
 # Using personal access token to GitHub Package container registry
-echo "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" | docker login ghcr.io -u taqwimonehub --password-stdin
+echo "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" | docker login ghcr.io -u taqwimonehub --password-stdin
 
 # Push image ke GitHub Package container registry
 docker tag karsajobs-ui ghcr.io/taqwimonehub/karsajobs-ui:latest
