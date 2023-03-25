34383bc8e8b49639228ee57c58903a3fcc83338b
taqwimOnehub <taqwimcore@outlook.com>
2023-03-25 19:40:40 +0700

    add build push

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-
 # Build Docker image dari Dockerfile
 docker build -t karsajobs:latest .
 
 # Using personal access token to GitHub Package container registry
-echo "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" | docker login ghcr.io -u taqwimonehub --password-stdin
+echo $PASSWORD_GITHUB | docker login ghcr.io -u taqwimonehub --password-stdin
+
+# set tag images to latest
+docker tag karsajobs ghcr.io/taqwimonehub/karsajobs:latest
 
 # Push image ke GitHub Package container registry
-docker tag karsajobs ghcr.io/taqwimonehub/karsajobs:latest
 docker push ghcr.io/taqwimonehub/karsajobs
